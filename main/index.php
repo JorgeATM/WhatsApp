e894047d31e256b8934ad4b86fbba11feade0ba4
Vvictorvaldezz <61363942+Vvictorvaldezz@users.noreply.github.com>
2022-03-17 17:35:59 -0600

    n

--- main/index.php
+++ main/index.php
@@ -5,6 +5,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="/css/style.css">
+	<script
+        src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+        crossorigin="anonymous">
+    </script> 
 	<title>WhatsApp</title>
 </head>
 <body>
@@ -16,50 +21,27 @@
 		  <input id="email" type="text" placeholder="Mensaje" name="txtMensaje">
   <input id="submit" type="submit" value="Enviar">
   
+  <div class="formgroup" id="message-form">        
+    </div>        
+        <div class="formgroup" id="message-form">
+        <label >Mensajes recibidos: </label>
+        <div id="tablaUpdate"></div>
+        </div>
   
 </form>
 
-<!--Tabla Alumno-->
-                            
-<table class="table table-striped">
-	<thead>
-	  <tr>
-		  <th scope="col">e|m</th>
-		  <th scope="col">re</th>
-		  <th scope="col">texto</th>
-		  <th scope="col">fecha</th>
-		  <th scope="col">estado</th>
-		  
-	</thead>
-	<tbody>
-		
-		 
-	<?php
-		  include("../conexion/conexion.php"); //importar la conexion
-		  
-		  $sentencia = "SELECT em,re,texto,fecha,estado FROM servidor WHERE
-		  em = '1111111'";
-  
-		  $resultado = mysqli_query($conexion, $sentencia);
-  
-		  while($ver = mysqli_fetch_assoc($resultado)){
-			  echo"
-			  <tr>
-				  <td>".$ver["em"]."</td>
-				  <td>".$ver["re"]."</td>
-				  <td>".$ver["texto"]."</td>
-				  <td>".$ver["fecha"]."</td>
-				  <td>".$ver["estado"]."</td>
-				  
-				  
-			  </tr>";
-		  } 
-		  mysqli_close($conexion);                             
-	  ?>
+<script src="./js/funcion.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            setInterval(
+                function(){
+                    $('#tablaUpdate').load('table.php');
+                },2000
+            );
+        });
+    </script>
 
 
-	</tbody>
-  </table>
 </body>
 </html>
 
---
+++ main/table.php
@@ -0,0 +1,32 @@
+<table class="table table-striped" id="tb">
+    <thead>
+        <tr>
+        <th scope="col">em</th>
+		  <th scope="col">re</th>
+		  <th scope="col">texto</th>
+		  <th scope="col">fecha</th>
+		  <th scope="col">estado</th>
+    </thead>
+    <tbody>
+        <?php
+        include("../conexion/conexion.php"); //importar la conexion
+        $sentencia = "SELECT em,re,texto,fecha,estado FROM servidor WHERE
+		  em = '1111111'";
+
+        $resultado = mysqli_query($conexion, $sentencia);
+        while($ver = mysqli_fetch_assoc($resultado)){
+            echo"
+            <tr>
+                <td>".$ver["em"]."</td>
+                <td>".$ver["re"]."</td>
+                <td>".$ver["texto"]."</td>
+                <td>".$ver["fecha"]."</td>
+                <td>".$ver["estado"]."</td>
+                
+                
+            </tr>";
+        } 
+        mysqli_close($conexion); 
+        ?>
+    </tbody>
+</table>
