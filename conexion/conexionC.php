771839244bfb195a4b665f719995a01d491d2203
Vvictorvaldezz <61363942+Vvictorvaldezz@users.noreply.github.com>
2022-03-18 16:29:52 -0600

    aa

--- conexion/conexionC.php
+++ conexion/conexionC.php
@@ -1,8 +1,8 @@
 <?php
-    $servidor = "192.168.1.186";
+    $servidor = "localhost";
     $usuario = "root";
     $password = "";
-    $db = "cliente";
+    $db = "";
 
     $conexion = mysqli_connect(
         $servidor,
--- funciones/insert.php
+++ funciones/insert.php
@@ -1,5 +1,6 @@
 <?php
     include("../conexion/conexion.php");
+    include("../conexion/conexionC.php");
 
     $emisor= $_POST['txtEmisor']; 
     $numero=$_POST['txtNumero'];
@@ -7,18 +8,32 @@
     date_default_timezone_set('America/Mexico_City');
     $fecha = date('m-d-Y h:i:s a', time());
 
+    $estado= $_POST['txtEstado'];
 
 
-    $sentencia ="INSERT INTO servidor VALUES(DEFAULT,'$emisor','$numero','$mensaje','$fecha','pendiente')";
+    $sentencia ="INSERT INTO servidor VALUES(DEFAULT,'$emisor','$numero','$mensaje','$fecha','$estado')";
             
             if(mysqli_query($conexion,$sentencia)){
+
+                $sentenciaC= "INSERT INTO contacto VALUES('$receptor','$nombreReceptor')";
+                if(mysqli_query($conexion,$sentenciaC)){
+
+                        $sentenciaE= "INSERT INTO emisor VALUES('$emisor','$informacion')";
+                if(mysqli_query($conexion,$sentenciaE)){
+                        $sentenciaM= "INSERT INTO mensaje VALUES(DEFAULT,'$numero','$mensaje','$fecha','0','$estado')";
+                if(mysqli_query($conexion,$sentenciaM)){
+                        
+                }
+                }
+                }
+
                     header("Location: ../main/index.php");
             }else{
                 echo "no";
             }
 
     
-    
+            mysqli_close($conexionC);
     
     mysqli_close($conexion);
 ?>
